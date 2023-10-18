2e9a4e2890acccb44b733c2f2606c97a89201001
Eric Charles <eric@datalayer.io>
2023-10-18 15:44:12 +0200

    chore: lint

--- src/jupyterlab/handler.ts
+++ src/jupyterlab/handler.ts
@@ -19,16 +19,13 @@ export async function requestAPI<T>(
     'jupyter_viewer', // API Namespace
     endPoint
   );
-
   let response: Response;
   try {
     response = await ServerConnection.makeRequest(requestUrl, init, settings);
   } catch (error: any) {
     throw new ServerConnection.NetworkError(error);
   }
-
   let data: any = await response.text();
-
   if (data.length > 0) {
     try {
       data = JSON.parse(data);
@@ -36,10 +33,8 @@ export async function requestAPI<T>(
       console.log('Not a JSON response body.', response);
     }
   }
-
   if (!response.ok) {
     throw new ServerConnection.ResponseError(response, data.message || data);
   }
-
   return data;
 }

f6f682f46ac26301347e72783ecabee497eae12f
echarles <eric@datalayer.io>
2023-12-17 14:53:38 +0100

    chore: jupyter-kernels -> jupyter-iam

--- src/tabs/AboutTab.tsx
+++ src/tabs/AboutTab.tsx
@@ -17,14 +17,14 @@ const AboutTab = (props: Props): JSX.Element => {
       </Box>
       <Box mt={3} style={{height: 350}}>
         {!egg ?
-          <img src="https://assets.datalayer.tech/releases/datalayer-0.2.0-omalley.png" onClick={e => setEgg(true)}/>
+          <img src="https://assets.datalayer.tech/releases/datalayer-0.3.0-black-snake_1024.png" onClick={e => setEgg(true)}/>
             :
           <ECharlesIcon size={300} onClick={e => setEgg(false)}/>
         }
       </Box>
       <Box>
-        <Link href="https://datalayer.tech/docs/releases/0.2.0-omalley" target="_blank">
-          <Text as="h4">Datalayer 0.2.0 O'Malley Release</Text>
+        <Link href="https://datalayer.tech/docs/releases/0.3.0-black-snake" target="_blank">
+          <Text as="h4">Datalayer 0.3.0 Black Snake Release</Text>
         </Link>
       </Box>
       <Box>

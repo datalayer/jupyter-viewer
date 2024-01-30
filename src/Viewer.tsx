06bf47f77b4e335dce3f7dfb77715d7f1aec3746
echarles <eric@datalayer.io>
2024-01-30 13:14:03 +0100

    deps: bump versions

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { ThemeProvider, BaseStyles, Box } from '@primer/react';
-import { UnderlineNav } from '@primer/react/drafts';
+import { UnderlineNav } from '@primer/react';
 import { JupyterLabAppAdapter } from '@datalayer/jupyter-react';
 import { EyesIcon } from '@datalayer/icons-react';
 import ViewerFormTab from './tabs/ViewerFormTab';
